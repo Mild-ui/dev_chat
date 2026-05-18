1aaac5d666f89b57f6dfa88031f64e85353c231c
Moise NIYOBUHUNGIRO <moisecoder011@gmail.com>
2026-05-18 21:35:40 +0200

    i changed database name used in code

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   port: parseInt(process.env.DB_PORT || '3306'),
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'devchat',
+  database: process.env.DB_NAME || 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
